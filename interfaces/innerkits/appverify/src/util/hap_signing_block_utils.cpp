43a93eb5e22960f76061b330fca6cd82ddb706f2
丛晓宇 <congxiaoyu@huawei.com>
2025-08-14 09:09:54 +0000

    update interfaces/innerkits/appverify/src/util/hap_signing_block_utils.cpp.
    Signed-off-by: 丛晓宇 <congxiaoyu@huawei.com>

--- interfaces/innerkits/appverify/src/util/hap_signing_block_utils.cpp
+++ interfaces/innerkits/appverify/src/util/hap_signing_block_utils.cpp
@@ -666,7 +666,7 @@ bool HapSigningBlockUtils::InitDigestPrefix(const DigestParameter& digestParam,
 
 bool HapSigningBlockUtils::HapVerifyParallelizationSupported()
 {
-    return OHOS::system::GetBoolParameter("const.appverify.hap_verify_parallel", true);
+    return OHOS::system::GetBoolParameter("const.appverify.hap_verify_parallel", false);
 }
 } // namespace Verify
 } // namespace Security
